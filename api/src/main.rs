73ce2c04898a04a5d512bbe52289e6d7a3677e40
Simon Huang <simon.huang@tum.de>
2023-09-12 13:30:34 +0200

    add simple ws implementation

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,4 +1,29 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix::{Actor, StreamHandler};
+use actix_web::{get, post, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web_actors::ws;
+
+struct MyWs;
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
+
+async fn index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
+    let resp = ws::start(MyWs {}, &req, stream);
+    println!("{:?}", resp);
+    resp
+}
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -21,6 +46,7 @@ async fn main() -> std::io::Result<()> {
             .service(hello)
             .service(echo)
             .route("/hey", web::get().to(manual_hello))
+            .route("/ws", web::get().to(index))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
