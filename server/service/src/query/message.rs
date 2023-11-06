a733261cd5a2079da5a9b885c7afc5ac6e8ed604
simonhng <simon.huang@tum.de>
2023-11-06 22:47:42 +0100

    clean up db calls

--- server/service/src/query/message.rs
+++ server/service/src/query/message.rs
@@ -1,33 +1,17 @@
 use crate::Query;
-use ::entity::prelude::Message;
-use sea_orm::{prelude::*, Condition, QueryOrder};
+use entity::prelude::Message;
+use sea_orm::{prelude::*, QueryOrder};
 
 impl Query {
-    pub async fn find_count_unread_messages_by_conversation_for_account(
-        db: &DbConn,
-        conversation: ::entity::conversation::Model,
-        user_id: Uuid,
-    ) -> Result<u64, DbErr> {
-        let condition = Condition::all()
-            .add(::entity::message::Column::ReadAt.is_null())
-            .add(::entity::message::Column::RecipientId.eq(user_id));
-
-        conversation
-            .find_related(Message)
-            .filter(condition)
-            .count(db)
-            .await
-    }
-
     pub async fn find_messages_by_conversation(
         db: &DbConn,
-        conversation: ::entity::conversation::Model,
-        params: Option<::entity::api::message_api::MessageQueryParams>,
-    ) -> Result<Vec<::entity::message::Model>, DbErr> {
+        conversation: entity::conversation::Model,
+        params: Option<entity::api::message_api::MessageQueryParams>,
+    ) -> Result<Vec<entity::message::Model>, DbErr> {
         let mut query = conversation
             .find_related(Message)
-            .order_by_asc(::entity::message::Column::CreatedAt)
-            .cursor_by(::entity::message::Column::CreatedAt);
+            .order_by_asc(entity::message::Column::CreatedAt)
+            .cursor_by(entity::message::Column::CreatedAt);
 
         if let Some(params) = params {
             let mut query_ref = &mut query;
