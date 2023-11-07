cd15df31e40649c004a4cc5e9dcd06085db002b6
simonhng <simon.huang@tum.de>
2023-11-07 14:57:55 +0100

    add reviews to database

--- server/migration/src/lib.rs
+++ server/migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20230915_172016_create_account;
 mod m20230915_210044_create_conversation;
 mod m20231104_162154_add_attachments_to_message;
+mod m20231107_134946_add_reviews;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20230915_172016_create_account::Migration),
             Box::new(m20230915_210044_create_conversation::Migration),
             Box::new(m20231104_162154_add_attachments_to_message::Migration),
+            Box::new(m20231107_134946_add_reviews::Migration),
         ]
     }
 }
---
+++ server/migration/src/m20231107_134946_add_reviews.rs
@@ -0,0 +1,61 @@
+use sea_orm_migration::prelude::*;
+
+use crate::m20230915_172016_create_account::Account;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[derive(DeriveIden)]
+pub enum Review {
+    Table,
+    Id,
+    ReviewerId,
+    RecipientId,
+    Score,
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Review::Table)
+                    .if_not_exists()
+                    .col(
+                        ColumnDef::new(Review::Id)
+                            .uuid()
+                            .extra("DEFAULT uuid_generate_v4()")
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Review::ReviewerId).uuid().not_null())
+                    .col(ColumnDef::new(Review::RecipientId).uuid().not_null())
+                    .col(ColumnDef::new(Review::Score).tiny_integer().not_null())
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("reviewer_id")
+                            .from(Review::Table, Review::ReviewerId)
+                            .to(Account::Table, Account::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("recipient_id")
+                            .from(Review::Table, Review::RecipientId)
+                            .to(Account::Table, Account::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Review::Table).to_owned())
+            .await
+    }
+}
